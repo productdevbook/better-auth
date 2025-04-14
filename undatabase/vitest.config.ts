354f3e945ed5b1d9195e1f7f5b38058a0037a9cb
productdevbook <hi@productdevbook.com>
2025-04-14 22:25:03 +0300

    refactor: update reporters configuration for clarity

--- undatabase/vitest.config.ts
+++ undatabase/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    // reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
+    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json-summary', 'json'],
